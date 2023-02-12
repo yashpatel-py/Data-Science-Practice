2ef8b8d8643110bf2df958a081058a5e2e03caa5
yashpatel-py <mailforyash04@gmail.com>
2023-02-12 09:49:58 +0530

    Variables in python done

---
+++ Week 1 and Week 2/Python/1_variables.py
@@ -0,0 +1,64 @@
+# Assigning Simple variable
+# a = 10
+# print(a)
+
+# ----------------------------------------------------------------
+# Assigning different data types variables
+# An integer assignment
+# age = 22
+ 
+# A floating point
+# salary = 1456.8
+ 
+# A string
+# name = "Yash"
+ 
+# print(age)
+# print(salary)
+# print(name)
+
+# ----------------------------------------------------------------
+# An integer assignment
+# age = 22
+ 
+# A floating point
+# salary = 1456.8
+ 
+# A string
+# name = "Yash"
+ 
+# print(age)
+# print(salary)
+# print(name)
+
+# ----------------------------------------------------------------
+# Assigning a single value to multiple variables
+# a = b = c = 21
+# print(a)
+# print(b)
+# print(c)
+
+# ----------------------------------------------------------------
+# Assigning different values to multiple variables
+# a, b, c = "Yash Patel", 210.04, 21
+# print(a)
+# print(b)
+# print(c)
+
+# ----------------------------------------------------------------
+# same variavle with different types 
+# a = 10
+# a = "Yash Patel"
+# print(a)
+
+# ----------------------------------------------------------------
+# Global variables
+# x = 15
+# def change():
+#     # using a global keyword
+#     global x
+#     # increment value of a by 5
+#     x = x + 5
+#     print("Value of x inside a function :", x)
+# change()
+# print("Value of x outside a function :", x)
