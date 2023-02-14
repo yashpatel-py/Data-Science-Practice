28077023d43b634d169b6e97ba5736f45525ee9c
yashpatel-py <mailforyash04@gmail.com>
2023-02-14 18:51:49 +0530

    Python - String - DataScience

---
+++ Week 1 and Week 2/Python/3_string/3_strings.py
@@ -0,0 +1,61 @@
+# simple string
+# print("Hello")
+
+# Assigning the string to a variable
+# a = "Hello World!"
+# print(a)
+
+# ----------------------------------------------------------------
+# Multiline Sring
+# a = '''
+# Lorem 
+# ipsum 
+# dolor 
+# sit 
+# amet, 
+# consectetur 
+# adipiscing
+# '''
+# print(a)
+
+# a = """
+# Lorem 
+# ipsum 
+# dolor 
+# sit 
+# amet, 
+# consectetur 
+# adipiscing
+# """
+# print(a)
+
+# ----------------------------------------------------------------
+# String are arrays
+# a = "Hello, world!"
+# print(a[1])
+
+# ----------------------------------------------------------------
+# Looping Through a String
+# for a in "banana":
+#     print(a)
+
+# ----------------------------------------------------------------
+# String Length
+# a = "Hello, world!"
+# print(len(a))
+
+# ----------------------------------------------------------------
+# check words string
+# txt = "Yash patel is very good boy!"
+# print("Yash" in txt)
+
+# if "Yash" in txt:
+#     print("yes, 'Yash' is present in the string!")
+
+# ----------------------------------------------------------------
+# check words if not present in string
+# txt = "Yash patel is very good boy!"
+# print("expensive" not in txt)
+
+# if "expensive" not in txt:
+#     print("yes, 'expensive' is not present in the string!")
