1f129d50329142509050c595a87679282f03c034
yashpatel-py <mailforyash04@gmail.com>
2023-02-14 18:53:08 +0530

    some file removed

--- Week 1 and Week 2/Python/Tuple/7_tuple_methods.py
+++
@@ -1,9 +0,0 @@
-# count()
-# the_tuple = (1, 1.2, 23j, 'Yash', True, 'Patel')
-# x = the_tuple.count(23j)
-# print(x)
-
-# index()
-the_tuple = (1, 1.2, 23j, 'Yash', True, 'Patel')
-x = the_tuple.index(23j)
-print(x)
