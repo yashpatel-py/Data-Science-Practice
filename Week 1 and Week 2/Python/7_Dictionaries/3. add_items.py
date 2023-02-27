753a9a1d4459dfcc635bdfdadc5339deeadbadbf
yashpatel-py <mailforyash04@gmail.com>
2023-02-27 15:36:26 +0530

    Python - Dictionary - DataScience

---
+++ Week 1 and Week 2/Python/7_Dictionaries/3. add_items.py
@@ -0,0 +1,13 @@
+dict1 = {
+    'name': 'Yash Patel',
+    'age': 23,
+    'Hobby': 'coding'
+}
+
+# update()
+# dict1.update({'youtube': "Django World"})
+# print(dict1)
+
+# change value in dict
+dict1.update({'age': 18})
+print(dict1)
---
+++ Week 1 and Week 2/Python/7_Dictionaries/4. remove_items.py
@@ -0,0 +1,17 @@
+dict1 = {
+    'name': 'Yash Patel',
+    'age': 23,
+    'Hobby': 'coding'
+}
+
+# pop()
+# dict1.pop('age')
+# print(dict1)
+
+# popitem()
+# dict1.popitem()
+# print(dict1)
+
+# del()
+del dict1['Hobby']
+print(dict1)
