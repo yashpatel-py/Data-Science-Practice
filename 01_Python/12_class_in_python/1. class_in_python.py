a2291c82f9d192547ce7210620c87b79f6d0a6db
yashpatel-py <mailforyash04@gmail.com>
2023-03-05 23:35:04 +0530

    Python - Class / Object - DataScience

--- 01_Python/12_class_in_python/1. class_in_python.py
+++ 01_Python/12_class_in_python/1. class_in_python.py
@@ -1,26 +1,35 @@
+# creating simple class
+# class MyClass:
+#     x = 5
+
+# onj1 = MyClass()
+# print(onj1.x)
+
 class my_class:
     def __init__(self, name, age):
         self.name = name
         self.age = age
     
-    def my_func(self):
-        print('My name is ' + self.name)
+#     def my_func(self):
+#         print('My name is ' + self.name)
     
-    def my_age(self):
-        print('My age is ', self.age)
+#     def my_age(self):
+#         print('My age is ', self.age)
 
-# Creating object of a class
+# # Creating object of a class
 obj1 = my_class("yash patel", 23)
+print(obj1)
 
-obj1.name = "Manav"
-obj1.age = 21
+# print(obj1.name)
+# print(obj1.age)
 
-del obj1
+# obj1.name = "Manav"
+# obj1.age = 21
 
-obj1.my_func()
-obj1.my_age()
+# del obj1
 
-
+# obj1.my_func()
+# obj1.my_age()
 
 # print(obj1.name)
 # print(obj1.age)
