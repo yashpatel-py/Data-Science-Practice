6b021352945c5a2c5e389db4b720b98a992275df
yashpatel-py <mailforyash04@gmail.com>
2023-03-06 10:29:09 +0530

    Python - Try/Except - DataScience

--- 01_Python/11_try_except/try_except.py
+++ 01_Python/11_try_except/try_except.py
@@ -1,9 +1,20 @@
-try:
-  x = 1
-  print(x)
-except NameError:
-  print("Variable x is not defined")
-except:
-  print("Something else went wrong")
-else:
-  print("Nothing went wrong")
+# try:
+#   x = 1
+#   print(x)
+# except NameError:
+#   print("Variable x is not defined")
+# except:
+#   print("Something else went wrong")
+# else:
+#   print("Nothing went wrong")
+
+# try:
+#   print(x)
+# except NameError:
+#   print("Variable x is not defined")
+# finally:
+#   print("Passed through try and except ")
+
+x = 5
+if x < 10:
+  raise Exception("The number is below 10")
